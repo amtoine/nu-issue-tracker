e98478e2aa9f0720f4602c1f1107d63b8dc308ea
amtoine <stevan.antoine@gmail.com>
2023-04-13 20:04:55 +0200

    add more labels to the plotting script

--- plot.py
+++ plot.py
@@ -5,11 +5,14 @@ import sys
 if __name__ == "__main__":
     repo = sys.argv[1]
     data = json.loads(sys.argv[2])
+    date = sys.argv[3]
 
-    dates = [value["date"] for value in data]
+    dates = [value["when"] for value in data]
     issues = [value["issues"] for value in data]
     plt.plot(dates, issues, label=f"{repo}")
 
+    plt.xlabel(f"time in days: 0 is the current date ({date})")
+    plt.ylabel("number of issues in the tracker")
     plt.title("number of issues over time")
     plt.legend()
     plt.show()
